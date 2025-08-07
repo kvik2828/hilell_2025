90600e801994c332a4b8549e978a188ba8aea580
kvik.2828 <kvik.2828@gmail.com>
2025-08-07 13:50:41 +0300

    калькулятор

--- Lesson3/HW_3.1.py
+++ Lesson3/HW_3.1.py
@@ -1,24 +1,24 @@
-n1= int(input("Please enter the number1: "))
+n1 = int(input("Please enter the number1: "))
 print(n1)
-n2= int(input("Please enter the number2: "))
+
+n2 = int(input("Please enter the number2: "))
 print(n2)
-MO=input("Please enter mathematical operation: +, -, *, /")
-# what mathematical operation
-if MO=="+":
-    print(n1+n2)
-else:
-    if MO=="-":
-        print(n1-n2)
+
+MO = input("Please enter mathematical operation: +, -, *, /")
+
+if MO == "+":
+    print(n1 + n2)
+
+elif MO == "-":
+    print(n1 - n2)
+
+elif MO == "*":
+    print(n1 * n2)
+
+elif MO == "/":
+    if n2 != 0:
+        print(n1/n2)
     else:
-        if MO=="*":
-            print(n1*n2)
-        else:
-            if MO == "/":
-                if n2!=0:
-                    print(n1/n2)
-                else:
-                    print("division_by_zero")
+        print("division_by_zero")
+
 print("end")
-
-
-
