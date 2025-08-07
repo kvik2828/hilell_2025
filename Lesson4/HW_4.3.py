0f8ec4ed8725cf5766bd87aa7ee600051707e79a
kvik.2828 <kvik.2828@gmail.com>
2025-08-07 12:55:52 +0300

    Список із 3 елементів

--- Lesson4/HW_4.3.py
+++ Lesson4/HW_4.3.py
@@ -2,16 +2,7 @@ import random
 
 
 lst = [random.randint(3, 10) for i in range(random.randint(1, 10))]
-el_1 = lst[0]
 
-while len(lst) <= 3:
-    el_2 = lst[-1]
-    break
-else:
-    el_2 = lst[2]
-
-el_3 = lst[-2]
-
-lst_new = [el_1,el_2,el_3]
+lst_new = [lst[0],lst[2],lst[-2]]
 
 print(lst_new)
