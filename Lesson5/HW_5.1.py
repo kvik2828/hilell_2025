24360b8d86e17ab66b8850fd1ce1998adfc50104
kvik.2828 <kvik.2828@gmail.com>
2025-08-10 16:37:14 +0300

    lesson 5

--- Lesson5/HW_5.1.py
+++ Lesson5/HW_5.1.py
@@ -1,26 +1,27 @@
 import string
-print(string.punctuation)
 import keyword
-print(keyword.kwlist)
 
 
 user_input = str(input("Please enter name variable: "))
+
 first_simb = user_input[0]
 simbol_two = user_input.count('_')
-input_no_punctuation = user_input.isalnum() or bool(simbol_two)
-
+first_simb_no_number = first_simb.isalpha() or bool(simbol_two)
+input_no_punctuation = user_input not in string.punctuation or bool(simbol_two)
 input_no_keyword = user_input not in keyword.kwlist
 
-first_simb_no_number = first_simb.isalpha() or bool(simbol_two)
-
-low_regist = (user_input.islower()) and bool(simbol_two)
-
-if simbol_two == 0:
+if simbol_two == 1:
     INP = True
-elif simbol_two == 1:
+elif  simbol_two == 0:
     INP = True
-elif simbol_two >= 2:
+else:
     INP = False
 
-print(first_simb_no_number and (user_input.islower() or bool(simbol_two)) and INP and not input_no_keyword and input_no_punctuation)
+if simbol_two == 1:
+    low_registr = True
+else:
+    low_registr = (user_input.islower())
+
+
+print(first_simb_no_number and (user_input.islower() or bool(simbol_two)) and INP and input_no_keyword and input_no_punctuation)
 
--- Lesson5/HW_5.3.py
+++ Lesson5/HW_5.3.py
@@ -1,7 +1,16 @@
-my_string = 'I like Python'
+import string
 
-def string_to_hashtag(my_string)
+user_input = input(str("Please enter name: "))
+user_input = user_input.title()
+punct = str(string.punctuation)
 
+punct_lst = list(punct)
 
+chars_to_replace = punct_lst
+for char in chars_to_replace:
+    user_input = user_input.replace(char, ' ')
 
+user_input_strip = "".join(user_input.split())
 
+result = "#" + user_input_strip
+print(result[:139])
