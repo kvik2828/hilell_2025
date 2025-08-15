dd4b27be9367348a24c90cbbf8afa75816b6ad11
kvik.2828 <kvik.2828@gmail.com>
2025-08-15 15:19:08 +0300

    lesson 8

---
+++ Lesson8/HW_8.2.py
@@ -0,0 +1,17 @@
+def is_palindrome(text):
+
+    sentence = ''
+    for char in text:
+        if char.isalnum():
+            sentence += char.lower()
+
+    reversed_sentence = ''.join(reversed(sentence))
+
+    return sentence == reversed_sentence
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+
+print("ОК")
