63cc9358b5c54baab6ad4d33eaf14f534f0b1472
kvik.2828 <kvik.2828@gmail.com>
2025-08-15 13:48:11 +0300

    lesson 7

--- Lesson7/HW_7.2.py
+++ Lesson7/HW_7.2.py
@@ -10,4 +10,5 @@ assert correct_sentence("hello") == "Hello.", 'Test2'
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
 assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
 assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
 print('ОК')
